6948877f8b40abeb5e6fea643e8618beb9822df4
blumbang <blmbng.id@gmail.com>
2026-05-02 22:50:17 +0400

    Delete functions/blog/[slug].js

--- functions/blog/[slug].js
+++
@@ -1,6 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  url.pathname = '/blog/post.html';
-  const request = new Request(url.toString(), context.request);
-  return fetch(request);
-}

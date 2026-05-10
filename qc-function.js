334a4c7485a4d5918111b60e6714b10c1a13355b
blumbang <blmbng.id@gmail.com>
2026-05-10 18:27:03 +0400

    Delete qc-function.js

--- qc-function.js
+++
@@ -1,6 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  url.pathname = '/id.html';
-  const response = await context.env.ASSETS.fetch(url.toString());
-  return response;
-}
